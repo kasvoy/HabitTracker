2720b24c22d5440b14766fbf35ba7ebc7e451d1d
kasvoy <woj.kasperczyk@gmail.com>
2023-01-18 17:51:24 +0400

    fixed run script

--- habit.py
+++ habit.py
@@ -4,9 +4,9 @@ os = sys.platform
 
 if os == 'win32':
     if len(sys.argv) == 2 and sys.argv[1].lower() == 'test':
-        subprocess.run("python -m src.main", shell = True)
-    else:
         subprocess.run("python -m src.main test", shell = True)
+    else:
+        subprocess.run("python -m src.main", shell = True)
 
 elif os == 'linux' or os == 'darwin':
     if len(sys.argv) == 2 and sys.argv[1].lower() == 'test':
